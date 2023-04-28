73c52aab72b4c740dd8f792b5a6d644ffe0c9ac9
imran8104538267 <132047643+imran8104538267@users.noreply.github.com>
2023-04-28 17:33:38 +0530

    Update streamlit_app.py
    added new breakfast

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,8 @@
 import streamlit
 streamlit.title('Hi myself imran')
+
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
